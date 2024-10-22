9f6395473ecd632accb415a13b68fcd33e5fd784
wilson-tan-gd <81664509+wilson-tan-gd@users.noreply.github.com>
2024-10-22 13:34:04 +0700

    Bug fixed object pooler

--- Assets/Scripts/Pooler/ProjectilePooler.cs
+++ Assets/Scripts/Pooler/ProjectilePooler.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ProjectilePooler : ObjectPooler
 {
+    public static ProjectilePooler instance;
     protected override void Awake()
     {
         base.Awake();
+
+        if (instance == null)
+        {
+            instance = this;
+        }
 
     }
 }
