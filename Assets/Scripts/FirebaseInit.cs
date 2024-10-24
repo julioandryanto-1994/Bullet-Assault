2652fd1d252201f90cd392e0600432bc4c1fff55
NP <146634457+Nandapp28@users.noreply.github.com>
2024-10-24 14:02:40 +0700

    Error Firebase database
    NP

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(continuationAction: task => 
+        {
+           FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
