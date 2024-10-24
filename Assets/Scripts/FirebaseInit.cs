c3019a1331d5ce84d3d295e0c17523d5ae4c749e
NP <146634457+Nandapp28@users.noreply.github.com>
2024-10-24 14:14:10 +0700

    Firebase init - fix

--- Assets/Scripts/FirebaseInit.cs
+++ Assets/Scripts/FirebaseInit.cs
@@ -10,9 +10,28 @@ public class FirebaseInit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(continuationAction: task => 
-        {
-           FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        /* FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(continuationAction: task => 
+         {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+         });*/
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+               var app = Firebase.FirebaseApp.DefaultInstance;
+                Debug.Log("Berhasil init");
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
         });
     }
 
